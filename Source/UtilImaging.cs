e500d620a76fd2af8e68afca462ce1fed476e61f
tocaner <tocaner@gmail.com>
2016-02-16 21:44:20 -0400

    Utility class for imaging operations

---
+++ Source/UtilImaging.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+
+namespace Utils
+{
+  class UtilImaging
+  {
+    public static void SaveImageAsJpeg(Image image, string fileName, long quality)
+    {
+      EncoderParameters encoderParams = new EncoderParameters(1);
+      encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+      ImageCodecInfo codecInfo = GetEncoderInfo(ImageFormat.Jpeg);
+      image.Save(fileName, codecInfo, encoderParams);
+    }
+
+
+    private static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+    {
+      ImageCodecInfo result = null;
+      ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+
+      for(int j = 0; (result == null) && (j < encoders.Length); j++)
+      {
+        if(encoders[j].FormatID == format.Guid)
+        {
+          result = encoders[j];
+        }
+      }
+      return result;
+    }
+  }
+}
