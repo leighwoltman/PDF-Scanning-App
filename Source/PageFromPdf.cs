48005b9b7e0883f9f51afe2462c90bceb86a649e
leighwoltman <leigh.woltman@gmail.com>
2016-03-18 14:04:45 -0300

    Adding The Start of More Generic PDF Import

---
+++ Source/PageFromPdf.cs
@@ -0,0 +1,98 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Utils;
+
+
+namespace Model
+{
+  public class PageFromPdf : Page
+  {
+    private Image myImageFile;
+    private bool fRasterize;
+    private string fFilename;
+    private long fPageNumber;
+
+    public PageFromPdf(string filename, long pageNumber, Image image)
+    {
+      fFilename = filename;
+      fPageNumber = pageNumber;
+
+      if(image == null)
+      {
+        fRasterize = true;
+        this.myImageFile = CreateImage();
+      }
+      else
+      {
+        fRasterize = false;
+        this.myImageFile = image;
+      }
+      
+      this.Size = PageSize.Letter;
+
+      InitializeImage();
+
+      // we want to save memory
+      this.myImageFile.Dispose();
+      this.myImageFile = null;
+    }
+
+
+    protected override Image CreateImage()
+    {
+      if(myImageFile != null)
+      {
+        return myImageFile;
+      }
+      else
+      {
+        if(fRasterize)
+        {
+          // we need to load it from the file
+          System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+          Stream myStream = myAssembly.GetManifestResourceStream("PDF_Scanner_App_WPF.PdfNotAvailableBanner.png");
+          Image image = Image.FromStream(myStream);
+
+          return image;
+        }
+        else
+        {
+          return PdfImporter.GetSingleImageFromPdfPage(PdfImporter.GetSinglePdfPageFromPdfDocument(fFilename, fPageNumber));
+        }
+      }
+    }
+
+
+    public override void AddPdfPage(PdfDocument pdfDocument)
+    {
+      if (fRasterize)
+      {
+        PdfDocument inputDocument = PdfReader.Open(fFilename, PdfDocumentOpenMode.Import);
+
+        // otherwise we just want to add the document from the original document
+        pdfDocument.AddPage(inputDocument.Pages[(int)fPageNumber]);
+      }
+      else
+      {
+        base.AddPdfPage(pdfDocument);
+      }
+    }
+
+    public override void CleanUp()
+    {
+      if (this.myImageFile != null)
+      {
+        this.myImageFile.Dispose();
+        this.myImageFile = null;
+      }
+
+      base.CleanUp();
+    }
+  }
+}
--- Source/PdfImporter.cs
+++ Source/PdfImporter.cs
@@ -20,101 +20,157 @@ namespace Model
       {
         PdfDocument pdfDocument = PdfReader.Open(filename);
 
+        long pageNumber = 0;
+
         // Iterate pages
         foreach(PdfPage page in pdfDocument.Pages)
         {
-          // Get resources dictionary
-          PdfDictionary resources = page.Elements.GetDictionary("/Resources");
-          if(resources != null)
+          Image image = PdfImporter.GetSingleImageFromPdfPage(page);
+          Page myPage = new PageFromPdf(filename, pageNumber, image);
+          document.AddPage(myPage);
+          
+          pageNumber++;
+        }
+      }
+      catch(Exception ex)
+      {
+        string msg = ex.Message;
+      }
+    }
+
+
+    static public PdfPage GetSinglePdfPageFromPdfDocument(string filename, long pageNumber)
+    {
+      PdfDocument pdfDocument = PdfReader.Open(filename);
+
+      return pdfDocument.Pages[(int)pageNumber];
+    }
+
+    static public Image GetSingleImageFromPdfPage(PdfPage page)
+    {
+      // determine what is on the page
+      long imagesFound = 0;
+      Image imageLargest = null;
+
+      // Get resources dictionary
+      PdfDictionary resources = page.Elements.GetDictionary("/Resources");
+      if(resources != null)
+      {
+        // Get external objects dictionary
+        PdfDictionary xObjects = resources.Elements.GetDictionary("/XObject");
+        if(xObjects != null)
+        {
+          ICollection<PdfItem> items = xObjects.Elements.Values;
+          // Iterate references to external objects
+          foreach(PdfItem item in items)
           {
-            // Get external objects dictionary
-            PdfDictionary xObjects = resources.Elements.GetDictionary("/XObject");
-            if(xObjects != null)
+            PdfReference reference = item as PdfReference;
+            if(reference != null)
             {
-              ICollection<PdfItem> items = xObjects.Elements.Values;
-              // Iterate references to external objects
-              foreach(PdfItem item in items)
+              PdfDictionary xObject = reference.Value as PdfDictionary;
+              // Is external object an image?
+              if(xObject != null && xObject.Elements.GetString("/Subtype") == "/Image")
               {
-                PdfReference reference = item as PdfReference;
-                if(reference != null)
+                PdfArray pdfArray = null;
+
+                try
                 {
-                  PdfDictionary xObject = reference.Value as PdfDictionary;
-                  // Is external object an image?
-                  if(xObject != null && xObject.Elements.GetString("/Subtype") == "/Image")
+                  pdfArray = xObject.Elements.GetArray("/Filter");
+                }
+                catch(Exception)
+                {
+                  // do nothing
+                }
+
+                if(pdfArray != null && pdfArray.Elements.Count == 2)
+                {
+                  // the "/Filter" field was an array
+
+                  // see if it had two values to indicate it is both JPEG and Deflate encoded
+                  if((pdfArray.Elements[0].ToString() == "/DCTDecode" && pdfArray.Elements[1].ToString() == "/FlateDecode") ||
+                      (pdfArray.Elements[1].ToString() == "/DCTDecode" && pdfArray.Elements[0].ToString() == "/FlateDecode"))
                   {
-                    PdfArray pdfArray = null;
+                    byte[] byteArray = xObject.Stream.Value;
 
-                    try
-                    {
-                      pdfArray = xObject.Elements.GetArray("/Filter");
-                    }
-                    catch(Exception)
-                    {
-                      // do nothing
-                    }
+                    FlateDecode fd = new FlateDecode();
+                    byte[] byteArrayDecompressed = fd.Decode(byteArray);
 
-                    if(pdfArray != null && pdfArray.Elements.Count == 2)
-                    {
-                      // the "/Filter" field was an array
+                    Image image = Image.FromStream(new MemoryStream(byteArrayDecompressed));
 
-                      // see if it had two values to indicate it is both JPEG and Deflate encoded
-                      if((pdfArray.Elements[0].ToString() == "/DCTDecode" && pdfArray.Elements[1].ToString() == "/FlateDecode") ||
-                          (pdfArray.Elements[1].ToString() == "/DCTDecode" && pdfArray.Elements[0].ToString() == "/FlateDecode"))
+                    imagesFound++;
+                    imageLargest = image;
+                  }
+                }
+                else
+                {
+                  string filter = xObject.Elements.GetString("/Filter");
+
+                  switch(filter)
+                  {
+                    case "/DCTDecode":
                       {
+                        // this is a directly encoded JPEG image
+                        byte[] byteArray = xObject.Stream.Value;
+
+                        Image image = Image.FromStream(new MemoryStream(byteArray));
+
+                        imagesFound++;
+                        imageLargest = image;
+                      }
+                      break;
+                    case "/FlateDecode":
+                      {
+                        // potientially this is a BMP/PNG image
                         byte[] byteArray = xObject.Stream.Value;
 
                         FlateDecode fd = new FlateDecode();
                         byte[] byteArrayDecompressed = fd.Decode(byteArray);
 
-                        Image image = Image.FromStream(new MemoryStream(byteArrayDecompressed));
+                        try
+                        {
+                          Image image = Image.FromStream(new MemoryStream(byteArrayDecompressed));
 
-                        Page myPage = new PageFromImage(image);
-                        document.AddPage(myPage);
+                          imagesFound++;
+                          imageLargest = image;
+                        }
+                        catch(Exception e)
+                        {
+                          // do nothing
+                        }
                       }
-                    }
-                    else
-                    {
-                      string filter = xObject.Elements.GetString("/Filter");
-
-                      switch(filter)
+                      break;
+                    case "/CCITTFaxDecode":
                       {
-                        case "/DCTDecode":
-                          {
-                            // this is a directly encoded JPEG image
-                            byte[] byteArray = xObject.Stream.Value;
+                        byte[] data = xObject.Stream.Value;
 
-                            Image image = Image.FromStream(new MemoryStream(byteArray));
+                        string fFilename = Path.GetTempFileName();
 
-                            Page myPage = new PageFromImage(image);
-                            document.AddPage(myPage);
-                          }
-                          break;
-                        case "/FlateDecode":
-                          {
-                            // potientially this is a BMP/PNG image
-                          }
-                          break;
-                        case "/CCITTFaxDecode":
-                          {
-                            byte[] data = xObject.Stream.Value;
+                        Tiff tiff = Tiff.Open(fFilename, "w");
 
-                            string fFilename = Path.GetTempFileName();
+                        tiff.SetField(TiffTag.IMAGEWIDTH, xObject.Elements.GetInteger("/Width"));
+                        tiff.SetField(TiffTag.IMAGELENGTH, xObject.Elements.GetInteger("/Height"));
+                        tiff.SetField(TiffTag.COMPRESSION, Compression.CCITTFAX4);
+                        tiff.SetField(TiffTag.BITSPERSAMPLE, xObject.Elements.GetInteger("/BitsPerComponent"));
+                        tiff.SetField(TiffTag.SAMPLESPERPIXEL, 1);
+                        tiff.WriteRawStrip(0, data, data.Length);
+                        tiff.Close();
 
-                            Tiff tiff = Tiff.Open(fFilename, "w");
-                            tiff.SetField(TiffTag.IMAGEWIDTH, xObject.Elements.GetInteger("/Width"));
-                            tiff.SetField(TiffTag.IMAGELENGTH, xObject.Elements.GetInteger("/Height"));
-                            tiff.SetField(TiffTag.COMPRESSION, Compression.CCITTFAX4);
-                            tiff.SetField(TiffTag.BITSPERSAMPLE, xObject.Elements.GetInteger("/BitsPerComponent"));
-                            tiff.SetField(TiffTag.SAMPLESPERPIXEL, 1);
-                            tiff.WriteRawStrip(0, data, data.Length);
-                            tiff.Close();
+                        Image image = Image.FromFile(fFilename);
 
-                            Page myPage = new PageFromFile(fFilename, PageSize.Letter);
-                            document.AddPage(myPage);
-                          }
-                          break;
+                        // once loaded, try and delete the temp file
+                        try
+                        {
+                          File.Delete(fFilename);
+                        }
+                        catch(Exception e)
+                        {
+                          // do nothing
+                        }
+
+                        imagesFound++;
+                        imageLargest = image;
                       }
-                    }
+                      break;
                   }
                 }
               }
@@ -122,10 +178,13 @@ namespace Model
           }
         }
       }
-      catch(Exception ex)
+
+      if( imagesFound != 1 )
       {
-        string msg = ex.Message;
+        imageLargest = null;
       }
+
+      return imageLargest;
     }
   }
 }
